7836555812ce814224614fa1799604df892d9cf4
Jojo Lin <maja0270558@gmail.com>
2018-02-05 16:57:27 +0800

    Test basic

--- week1/Swift Basic/basic1.playground/Contents.swift
+++ week1/Swift Basic/basic1.playground/Contents.swift
@@ -2,4 +2,28 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var str = "basic a"
+let str2 = "basic b"
+let isTrue = true
+var isFloat = 3.0
+let 🐶🐮 = "dog"
+let SSTTRR =  str + str2
+
+let tuple: (Int, Int) = (1, 2)
+tuple.0
+tuple.1
+
+var a = 1
+a += 2
+
+(1, "zebra") < (2, "apple")
+//Range
+for index in 1...6{
+    print(index)
+}
+let array = ["Django", "Jojo","Bobo","Amber"]
+//Half-Open Range Operator
+for index in array[2...]{
+    print(index)
+}
+let range = ...5
